37947af2da184661c66663cb5139aa32ee1bd54b
Nguyen Dac Tai <44217992+msc0d3@users.noreply.github.com>
2024-09-24 10:41:40 +0700

    finish frontend

---
+++ LinkedinCheckerTools/ViewModel/FMainDatagridViewCellName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedinCheckerTools.ViewModel
+{
+    public class FMainDatagridViewCellName
+    {
+        public const string Cellstt = "clstt";
+        public const string Cellemail = "clemail";
+        public const string CellProxy = "clproxy";
+        public const string CellLinkedObj = "cllinkedObj";
+        public const string CellStatus = "clstatus";
+    }
+}
