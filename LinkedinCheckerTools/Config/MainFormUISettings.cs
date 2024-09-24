44e8f4319dcd63097d9fda3a5bc518aacc7d9a9d
Nguyen Dac Tai <44217992+msc0d3@users.noreply.github.com>
2024-09-24 10:09:13 +0700

    update

---
+++ LinkedinCheckerTools/Config/MainFormUISettings.cs
@@ -0,0 +1,14 @@
+﻿using LinkedinCheckerTools.Singleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedinCheckerTools.Config
+{
+    public class MainFormUISettings
+    {
+        public static IniFile MainFormUISettingsProvider = new IniFile(PathSingleton.MainFormUISettingPath);
+    }
+}
